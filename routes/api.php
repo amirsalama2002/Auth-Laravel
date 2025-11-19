d886784e0b041dc6b383d6c016c8d8cf41886ef6
Amir Salama <hassanamirsalama.com>
2025-11-19 22:11:43 +0200

    Auth

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use App\Http\Controllers\Api\UserAuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::post('register',[UserAuthController::class,'register']);
+Route::post('login',[UserAuthController::class,'login']);
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('logout',[UserAuthController::class,'logout']);
+
+});
+
+
+
+
+
+
